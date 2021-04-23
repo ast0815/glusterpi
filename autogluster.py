8a47de93704e430f2f0c1c89a362389295919ccd
Lukas Koch <lukas.koch@mailbox.org>
2021-04-23 18:16:23 +0100

    Deal with weird disk errors.

--- autogluster.py
+++ autogluster.py
@@ -4,13 +4,15 @@ from os import path
 from glob import glob
 import sh
 
+
 def mountpoint(device):
     """Return gluster-friendly mounting point of a device path."""
     name = path.basename(device)
     split = name.split("-")
     i = split.index("usb")
-    mount = path.join("/mnt/usb", split[i+1]).replace(":", "..")
+    mount = path.join("/mnt/usb", split[i + 1]).replace(":", "..")
     return mount
+
 
 # Get available USB drives
 drives = glob("/dev/disk/by-path/*-usb-*part1")
@@ -37,14 +39,18 @@ for mount in mounted:
         except Exception as e:
             pass
 
-        try:
-            sh.rmdir(mount)
-        except Exception as e:
-            pass
+    try:
+        sh.rmdir(mount)
+    except Exception as e:
+        pass
 
 # Mount all present drives
 for drive in drives:
     mount = mountpoint(drive)
+    # Abort if folder already exists
+    if path.exists(mount):
+        print(f"{mount} already exists!")
+        continue
     sh.mkdir("-p", mount)
     sh.chmod("go-w", mount)
     print(f"Mounting {drive} to {mount}")
@@ -55,4 +61,12 @@ for drive in drives:
 
 # Restart the gluster processes
 print(f"Starting gluster bricks")
-sh.gluster.volume.start("gvol1", "force")
+try:
+    sh.gluster.volume.start("gvol1", "force")
+except Exception as e:
+    print(e.stderr)
+    for word in e.stderr.split():
+        for mount in mounted:
+            if mount in str(word):
+                print(f"Unmounting {mount}")
+                sh.umount(mount)
